1a58386617add12af9734949e3d0086162aab9ab
Breno Tainan <brenotaguiar@gmail.com>
2023-04-02 20:56:56 -0300

    camada service: deposito na conta

---
+++ app/backend/src/services/account.service.js
@@ -0,0 +1,18 @@
+const { Account } = require('../database/models');
+
+const accountService = {
+  deposit: async ({ balance }, id) => {
+    const account = await Account.findOne({
+      where: { id },
+      attributes: ['balance'],
+    });
+
+    if (!account) return undefined;
+
+    const newBalance = await account.update({ balance });
+
+    return newBalance;
+  },
+};
+
+module.exports = accountService;
