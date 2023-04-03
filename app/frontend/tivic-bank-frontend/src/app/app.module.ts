cae16edec2330afc366c3cc538d7c7b39256abb0
Breno Tainan <brenotaguiar@gmail.com>
2023-04-03 20:54:08 -0300

    configurando header

--- app/frontend/tivic-bank-frontend/src/app/app.module.ts
+++ app/frontend/tivic-bank-frontend/src/app/app.module.ts
@@ -6,18 +6,35 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './componentes/template/header/header.component';
 
+import { MatSidenavModule } from  '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './views/home/home.component';
+import { WithdrawalComponent } from './views/withdrawal/withdrawal.component';
+import { DepositComponent } from './views/deposit/deposit.component';
+import { BalanceComponent } from './views/balance/balance.component';
+import { TransactionComponent } from './componentes/template/transaction/transaction.component';
+import { NavComponent } from './componentes/template/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    WithdrawalComponent,
+    DepositComponent,
+    BalanceComponent,
+    TransactionComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
